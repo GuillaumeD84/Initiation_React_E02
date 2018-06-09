a5e316fb27ea8e0afe441203f65ba0377777e091
GuillaumeD84 <guillaume.durand@outlook.fr>
2018-06-09 15:00:20 +0200

    add some CSS + todolist label and counter

--- js/todolist/todolist.js
+++ js/todolist/todolist.js
@@ -1,5 +1,8 @@
 const app = {
   init: function() {
+    // Je stocke #todo, la cible pour les éléments de l'application
+    app.todo = document.getElementById('todo');
+
     // Construction du formulaire
     app.createForm();
 
@@ -14,12 +17,36 @@ const app = {
    */
   createForm: function() {
     console.log('createForm');
+
+    // Construire le formulaire <form>
+    var form = document.createElement('form');
+
+    // Construire le champ pour le label de la tâche
+    var input = document.createElement('input');
+    input.id = 'todoInput';
+    input.placeholder = 'Ajouter une tâche';
+
+    // Ajouter le champ dans le formulaire
+    form.appendChild(input);
+
+    // Ajouter le formulaire dans le DOM
+    app.todo.appendChild(form);
   },
   /**
    * Construction du compteur
    */
   createCount: function() {
     console.log('createCount');
+
+    // DOM (création du compteur)
+    var counter = document.createElement('div');
+    counter.id = 'todoCounter';
+
+    // Ajouter du contenu
+    counter.textContent = '2 tâches en cours';
+
+    // Ajout du compteur à la #todo
+    app.todo.appendChild(counter);
   },
   /**
    * Construction de la liste de tâches
